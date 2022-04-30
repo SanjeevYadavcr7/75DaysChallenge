cfa233dc0a327abd114f310768d8cace47dc191a
Sanjeev <sanjeevyadavcr7@gmail.com>
2022-04-30 16:54:38 +0530

    Kth Largest

--- Day38/Kth Largest.cc
+++ Day38/Kth Largest.cc
@@ -2,6 +2,38 @@
 
 class Solution {
 public:
+	// Approach: Using quickSelect method
+	
+	static int getPivot(int l, int r, vector<int>& nums){
+		int i = l-1;
+		for(int j=l; j<r; j++){
+			if(nums[j] <= nums[r]){
+				i++;
+				swap(nums[i],nums[j]);
+			}
+		}
+		swap(nums[i+1],nums[r]);
+		return i+1;
+	}
+
+	static int quickSelect(int l, int r, int k, vector<int>& nums) {
+		if(l <= r){
+			int pivot = getPivot(l,r,nums);
+			if(pivot == n-k) return nums[pivot];
+			else if(pivot > n-k) return quickSelect(l,pivot-1,k,nums);
+			else return quickSelect(pivot+1,r,k,nums);
+		}
+		return -1;
+	}
+
+	static int findKthSmallest(vector<int>& nums, int k) {
+		int n = nums.size();
+		return quickSelect(0,n-1,k,nums);
+	}	
+	
+	
+	/* Approach: Using priority queue
+	
     int findKthLargest(vector<int>& nums, int k) {
         priority_queue<int,vector<int>,greater<>> minHeap;
             
@@ -11,6 +43,7 @@ public:
         }
     	return minHeap.top();
     }
+    */
 };
 
 int main() {
@@ -19,7 +52,7 @@ int main() {
 	vector<int> arr(n);
 	for(int i=0; i<n; i++) cin >> arr[i];
 	
-	int num = findKthLargest(arr,k);
+	int num = Solution::findKthSmallest(arr,k);
 	cout << num << endl;
 	
 	return 0;
