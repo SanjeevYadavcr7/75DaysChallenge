793a9268caf9c4314c4a5b98c75791bc55f70457
Sanjeev <sanjeevyadavcr7@gmail.com>
2022-05-12 21:08:23 +0530

    Q2 | TC: O(M*N) | SC: O(1)

---
+++ Day50/Minimum Path Sum.cc
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        for(int i=1; i<m; i++) grid[i][0] += grid[i-1][0];
+        for(int j=1; j<n; j++) grid[0][j] += grid[0][j-1];
+        
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) grid[i][j] += min(grid[i-1][j],grid[i][j-1]);
+        }
+        return grid[m-1][n-1];
+}
+
+int main() {
+	int n,m;
+	cin >> n >> m;
+	vector<vector<int>> grid(n,vector<int>(m));
+	for(int i=0; i<n; i++)
+		for(int j=0; j<m; j++) cin >> grid[i][j];
+	
+	int res = minPathSum(grid);
+	cout << res << endl;
+
+	return 0;
+}
